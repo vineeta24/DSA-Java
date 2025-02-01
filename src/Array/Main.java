05cd75982d0260cc35a22ddae0204e85edfd5be8
Vineeta Kande <vkande@idexcorp.com>
2025-02-01 23:29:13 +0530

    array data structure

---
+++ src/Array/Main.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class Main {
+    public static void main(String[] args) {
+        //Declaration of Array
+        int[] arr;
+
+        //Initialization of Array
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = new int[5];
+
+    }
+}
---
+++ src/Array/readme.md
@@ -0,0 +1,5 @@
+# Array
+
+**Array** is a collection of items of the same variable type that are stored at contiguous memory locations. 
+
+It is one of the most popular and simple data structures used in programming.
